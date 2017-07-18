c573d0a83f40ba83b2d2384f9932387e0d9e0f1c
Henrique Almeida Marcomini <henrique@(none)>
2017-07-18 13:15:57 -0300

    blablabla

--- login.php
+++ login.php
@@ -1,19 +1,2 @@
 <?php
-/*conecta com o banco*/
-/*Id:
- * 0 admin
- * 1 cuidador
- * 2 familiar
- * 3 paciente
- */
-if (isset($_POST['CPF']) && isset($_POST['tipo'])) 
-{
-    
-}
-else 
-{
-    echo 'Voce deve fornecer um usuario ou senha.';
-    header('Location: index.html');
-
-}
 ?>
