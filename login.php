98e44e9824c012de263ff6e3100761491269af37
Henrique Almeida Marcomini <henrique@(none)>
2017-07-18 12:58:00 -0300

    useless commit

--- login.php
+++ login.php
@@ -6,11 +6,30 @@
  * 2 familiar
  * 3 paciente
  */
-if (isset($_POST['usuario']) && isset($_POST['senha')) 
+if (isset($_POST['CPF']) && isset($_POST['tipo'])) 
 {
-    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) 
+    //Checa conexao
+    $connection = mysqli("localhost", "root", "reprovar", "prontuarioMedico");
+    switch($_POST['tipo'])
     {
-        setcookie("login",[$_POST['usuario'],$_POST['senha'],$id],time()+60*60*24*365, '/')
+        case "1":
+            $consulta = "SELECT CPF FROM `paciente` WHERE CPF = '".$_POST['CPF']."';";
+            break;
+        case "2":
+            $consulta = "SELECT CPF FROM `dadosCuidadoresProf` WHERE CPF = '".$_POST['CPF']."';";
+            break;
+        case "3":
+            $consulta = "SELECT CPF FROM `dadosCuidadoresNProf` WHERE CPF = '".$_POST['CPF']."';";
+            break;
+        case "3":
+            $consulta = "SELECT `CPFfamiliar` as CPF FROM `responsaveis` WHERE `CPFfamiliar` = '".$_POST['CPF']."';";
+            break;
+    }
+    //Faz a querry no banco de dados
+    $results = $connection->query($consulta);
+    if ($results->fetch_array()['CPF'] == $_POST['CPF']) 
+    {
+        setcookie("login",[$_POST['CPF'],$_POST['tipo']],time()+60*60*24*365, '/')
         header('Location: menu.html');
     }
     else
