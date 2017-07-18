9638646923f7bae41a6fcacbf25213b1be25d2b4
Henrique Almeida Marcomini <henrique@(none)>
2017-07-18 13:13:30 -0300

    blablabla

--- login.php
+++ login.php
@@ -11,23 +11,7 @@ if (isset($_POST['CPF']) && isset($_POST['tipo']))
     //Checa conexao
     $connection = mysqli("localhost", "root", "reprovar", "prontuarioMedico");
     $consulta = "";
-    switch($_POST['tipo'])
-    {
-        case "1":
-            $consulta = "SELECT CPF FROM `paciente` WHERE CPF = '".$_POST['CPF']."';";
-            break;
-        case "2":
-            $consulta = "SELECT CPF FROM `dadosCuidadoresProf` WHERE CPF = '".$_POST['CPF']."';";
-            break;
-        case "3":
-            $consulta = "SELECT CPF FROM `dadosCuidadoresNProf` WHERE CPF = '".$_POST['CPF']."';";
-            break;
-        case "4":
-            $consulta = "SELECT `CPFfamiliar` as CPF FROM `responsaveis` WHERE `CPFfamiliar` = '".$_POST['CPF']."';";
-            break;
-    }
-    //Faz a querry no banco de dados
-   
+
 }
 else 
 {
